bf292a517df9980bae8233ef84863f4f0747b420
Irfan <irfaanacar@gmail.com>
2023-10-01 23:35:41 +0300

    CSharp Windows Form Desktop folder transactions
    In this application, I tried to code features such as how to create a folder on the desktop, how to delete the folder, how to rename the folder, how to list the drivers and so on. By the way, Sorry for My English.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(277, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KLASÖR İŞLEMLERİ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(242, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "KLASÖR OLUŞTUR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(8, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "KLASÖRÜ SİL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkGray;
+            this.button3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(129, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "OLUŞTURMA TARİHİ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkGray;
+            this.button4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(129, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 64);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "AKTİF KLASÖR YOLUNU ÖĞREN";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkGray;
+            this.button5.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(647, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(228, 64);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "DOSYALARI LİSTELE";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkGray;
+            this.button6.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(647, 20);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(228, 64);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "SON ERİŞİM ZAMANI";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkGray;
+            this.button7.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(647, 90);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(228, 64);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "KLASÖR ADINI DEĞİŞTİR";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.DarkGray;
+            this.button8.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(647, 160);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(228, 64);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "SÜRÜCÜLERİ LİSTELE";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 11.2F, System.Drawing.FontStyle.Italic);
+            this.textBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Location = new System.Drawing.Point(129, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 41);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(427, 363);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(326, 212);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(469, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 51);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "SÜRÜCÜLERİ LİSTELE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(887, 587);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string klasör=textBox1.Text;
+            try
+            {
+                Directory.Delete("C:\\Users\\irfaa\\OneDrive\\Masaüstü\\" + klasör);
+                MessageBox.Show("KLASÖR BAŞARIYLA SİLİNDİ.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("KLASÖR SİLİNEMEDİ.");
+
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string klasör = textBox1.Text;
+            try
+            {
+                Directory.CreateDirectory("C:\\Users\\irfaa\\OneDrive\\Masaüstü\\" + klasör);
+                MessageBox.Show("KLASÖR BAŞARIYLA OLUŞTURULDU");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("KLASÖR OLUŞTURULAMADI.");
+
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string adres = "C:\\Users\\irfaa\\OneDrive\\Masaüstü\\GitHub Projects";
+            MessageBox.Show(Directory.GetCreationTime(adres).ToString());
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Directory.GetCurrentDirectory().ToString());
+            MessageBox.Show("ŞU ANDA AÇIK OLAN KLASÖRÜN YOLU BELİRTİLDİ");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string yol = "C:\\Users\\irfaa\\OneDrive\\Masaüstü\\GitHub Projects";
+            MessageBox.Show(Directory.GetLastAccessTime(yol).ToString());
+            MessageBox.Show("SON ERİŞİM ZAMANI BELİRTİLDİ");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Directory.Move("C:\\Users\\irfaa\\OneDrive\\Masaüstü\\ab", "C:\\Users\\irfaa\\OneDrive\\Masaüstü\\yeni ad bu");
+            MessageBox.Show("MASAÜSTÜNDE EĞER SİLİNMEDİYSE AB İSİMLİ KLASÖRÜN İSMİNİ 'YENİ AD BU' ŞEKLİNDE DEĞİŞTİRDİK.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "DOSYALARI LİSTELE";
+            string adres = "C:\\Program Files (x86)";
+            string[] klasor = Directory.GetDirectories(adres);
+            foreach(string j in klasor)
+            {
+                listBox1.Items.Add(j).ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label2.Text = "SÜRÜCÜLERİ LİSTELE";
+            string[] dizi = System.IO.Directory.GetLogicalDrives();
+            foreach (string i in dizi)
+            {
+                listBox1.Items.AddRange(dizi);
+            }
+
+        }
+    }
+}
